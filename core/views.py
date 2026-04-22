f003d394e40de874ef4b0cfcea0ee53db07647e4
crucioltheresa <mtcruciol@icloud.com>
2026-04-22 14:53:24 +0100

    Add base template and landing page

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+class IndexView:
+    def __call__(self, request):
+        return render(request, "index.html")
+
+
+index_view = IndexView()
