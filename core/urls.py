03205f74f478c049dc7680a659d49c03f6036c52
crucioltheresa <mtcruciol@icloud.com>
2026-04-23 14:41:51 +0100

    Refactor interest registration URLs and enhance button styles; add hero background and logo images

--- core/urls.py
+++ core/urls.py
@@ -9,8 +9,8 @@ from .views import (
 
 urlpatterns = [
     path("", IndexView, name="home"),
-    path("interest/", interest_registration_view, name="interest_registration"),
-    path("interest/success/", interest_success_view, name="interest_success"),
+    path("sign-up/", interest_registration_view, name="interest_registration"),
+    path("sign-up/success/", interest_success_view, name="interest_success"),
     path("admin/interests/", interest_list_view, name="interest_list"),
     path(
         "admin/interests/<int:registration_id>/update_status/",
