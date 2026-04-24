c4e0417d11994092601dd28ed1c36adb6c8e0dc1
crucioltheresa <mtcruciol@icloud.com>
2026-04-24 22:52:13 +0100

    Implement Seasons model, forms, views, and URLs; add admin registration and permissions

--- seasons/models.py
+++ seasons/models.py
@@ -1,3 +1,19 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
+
 # Create your models here.
+class Season(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    status = models.CharField(
+        max_length=100,
+        choices=[("draft", "Draft"), ("active", "Active"), ("finished", "Finished")],
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def clean(self):
+        if self.start_date and self.end_date and self.start_date <= self.end_date:
+            raise ValidationError({"End date mmust be after start date."})
---
+++ seasons/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (
+    seasons_list_view,
+    create_season,
+    edit_season,
+    delete_season,
+    season_detail,
+)
+
+urlpatterns = [
+    path("seasons/", seasons_list_view, name="seasons"),
+    path("seasons/create/", create_season, name="create_season"),
+    path("season/<int:season_id>/", season_detail, name="season_detail"),
+    path("seasons/<int:season_id>/edit/", edit_season, name="season_edit"),
+    path("seasons/<int:season_id>/delete/", delete_season, name="delete_season"),
+]
